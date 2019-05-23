71c7643319afd071f53f846857c3f210aa1f2951
bomberr1 <kurikinton.max@gmail.com>
2019-05-23 12:45:04 +0000

    basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
  def create
     @user = User.new(user_params)
     if @user.save
+        log_in @user
         flash[:success] = "ユーザーの新規作成に成功しました。" 
         redirect_to @user
       # 保存が成功した場合はここに記述した処理が実行される。
@@ -27,3 +28,5 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
+
+ 
