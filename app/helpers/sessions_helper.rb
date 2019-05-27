8f868a290138ecaee223fbb963eaebdec6ee91dc
bomberr1 <kurikinton.max@gmail.com>
2019-05-27 04:31:14 +0000

    Finish user edit index destroy

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,26 +1,41 @@
 module SessionsHelper
-    def log_in(user)
-        session[:user_id] = user.id
-    end
-    
-    def current_user
-     if session[:user_id]
-        @current_user ||= User.find_by(id: session[:user_id])
-    end 
+
+  # 引数に渡されたユーザーでログインする
+  def log_in(user)
+    session[:user_id] = user.id
   end
 
+  # 現在ログイン中のユーザーを返す (ただし、いる場合のみ)
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
 
+  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
 
-def logged_in?
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
     !current_user.nil?
-  end 
- 
- 
- def log_out
+  end
+
+  # 現在のユーザーをログアウトする
+  def log_out
     session.delete(:user_id)
     @current_user = nil
   end
+
+def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを記憶しておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
- 
-
-
+  
